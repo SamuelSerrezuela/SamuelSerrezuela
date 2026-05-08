eb783efd193042eeee86f02a4e0c2b4b5f6c7512
Alumno <alumno@et26.local>
2026-05-08 08:44:58 -0300

    subiendo actividad 10

---
+++ SamuelSerrezuela-Act10/SamuelSerrezuela-Act10/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto1
+/*Crear una matriz de 2 filas y 5 columnas. Realizar la carga de
+componentes por columna (es decir primero ingresar toda la primer
+columna, luego la segunda columna y así sucesivamente)
+Imprimir luego la matriz.*/
+
+
+
+{
+    class filas
+    {
+
+        static void Main()
+        {
+           
+            int filas = 2;
+            int columnas = 5;
+            int[,] matriz = new int[filas, columnas];
+
+            
+            CargarMatrizPorColumna(matriz);
+            ImprimirMatriz(matriz);
+        }
+
+        
+        static void CargarMatrizPorColumna(int[,] mat)
+        {
+            
+            for (int c = 0; c < mat.GetLength(1); c++)
+            {
+                
+                for (int f = 0; f < mat.GetLength(0); f++)
+                {
+                    Console.Write($"ingresa valor para (Fila {f}, Columna {c}): ");
+                    mat[f, c] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        
+        static void ImprimirMatriz(int[,] mat)
+        {
+            Console.WriteLine("\nvisualización de la matriz:");
+            for (int f = 0; f < mat.GetLength(0); f++)
+            {
+                for (int c = 0; c < mat.GetLength(1); c++)
+                {
+                    Console.Write(mat[f, c] + "\t");
+                }
+                Console.WriteLine(); 
+         
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
+
+
+
---
+++ SamuelSerrezuela-Act10/punto3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Crear una matriz de n * m filas (cargar n y m por teclado) Imprimir los
+cuatro valores que se encuentran en los vértices de la misma (mat[0][0]
+etc.)*/
+
+namespace punto3
+{
+    class Program
+    {
+
+        static int n, m;
+
+
+
+        public void Cargar(int[,] mat)
+        {
+            for (int f = 0; f < n; f++)
+            {
+                for (int c = 0; c < m; c++)
+                {
+                    Console.Write($"Dato [{f},{c}]: ");
+                    mat[f, c] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        public void Mostrar(int[,] mat)
+        {
+            Console.WriteLine("\nMatriz completa:");
+            for (int f = 0; f < n; f++)
+            {
+                for (int c = 0; c < m; c++)
+                {
+                    Console.Write(mat[f, c] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void MostrarVertices(int[,] mat)
+        {
+            Console.WriteLine("\nLos cuatro vértices son:");
+            Console.WriteLine("Arriba-Izquierda: " + mat[0, 0]);
+            Console.WriteLine("Arriba-Derecha: " + mat[0, m - 1]);
+            Console.WriteLine("Abajo-Izquierda: " + mat[n - 1, 0]);
+            Console.WriteLine("Abajo-Derecha: " + mat[n - 1, m - 1]);
+        }
+
+        public void Main()
+        {
+            Console.Write("Filas: ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Columnas: ");
+            m = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, m];
+
+            Cargar(matriz);
+            Mostrar(matriz);
+            MostrarVertices(matriz);
+
+
+            Console.ReadKey();
+        }
+    }
+}
